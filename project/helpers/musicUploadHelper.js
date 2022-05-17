b041ac33818f674169f0cb4825e556aeee1739f1
Mauro Santoni <44559498+mjsantoni@users.noreply.github.com>
2022-05-17 17:51:51 -0300

    Refactor music upload helper to use env vars

--- project/helpers/musicUploadHelper.js
+++ project/helpers/musicUploadHelper.js
@@ -5,13 +5,13 @@ export const musicUpload = async (musicFile, musicTitle) => {
   // Set the configuration
   // TODO: This config is to test. Replace with app's config object
   const firebaseConfig = {
-    apiKey: "AIzaSyDMWhCaChTbBsE8KoOjGPlTRFLRioGsFyU",
-    authDomain: "media-upload-29cf6.firebaseapp.com",
-    projectId: "media-upload-29cf6",
-    storageBucket: "media-upload-29cf6.appspot.com",
-    messagingSenderId: "55476725270",
-    appId: "1:55476725270:web:568b7f058923d5607e3737",
-    measurementId: "G-VDPS24FER3",
+    apiKey: process.env.STORAGE_API_KEY,
+    authDomain: process.env.STORAGE_AUTH_DOMAIN,
+    projectId: process.env.STORAGE_PROJECT_ID,
+    storageBucket: process.env.STORAGE_STORAGE_BUCKET,
+    messagingSenderId: process.env.STORAGE_MESSAGING_SENDER_ID,
+    appId: process.env.STORAGE_APP_ID,
+    measurementId: process.env.STORAGE_MEASUREMENT_ID,
   };
   initializeApp(firebaseConfig);
   const storage = getStorage();
