8c2f0f285273c70064854f6c1115252e06bc1563
Mauro Santoni <mausantoni@gmail.com>
2022-06-09 12:33:24 -0300

    Add tests and swagger docs for locations

--- project/routes/worldLocationsRouter.js
+++ project/routes/worldLocationsRouter.js
@@ -1,9 +1,31 @@
 import express from "express";
 import worldLocations from "../helpers/worldLocations.js";
+import "./worldLocationsDoc.js";
 
 const locationsRouter = express.Router();
 const LOCATIONS_ROUTE = "/locations";
 
+/**
+ * @swagger
+ * tags:
+ *   name: World Locations
+ *   description: World Locations related endpoints.
+ */
+
+/**
+ * @swagger
+ * /locations:
+ *   get:
+ *     summary: Get all locations
+ *     tags: [World Locations]
+ *     description: Get all locations
+ *     responses:
+ *       '200':
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/WorldLocation'
+ */
 locationsRouter.get("/", (req, res) => {
   res.json(worldLocations);
 });
